cd8be8e605fea9b37aea8256b5854a094f52ffdf
Artem Pronchakov <artem.pronchakov@calisto.email>
2014-12-21 04:19:24 +0300

    Add ThreadLocal example

---
+++ Threads/ThreadLocalVariable/src/edu/javacourse/threads/ThreadLocalVariableExample.java
@@ -0,0 +1,20 @@
+package edu.javacourse.threads;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class ThreadLocalVariableExample {
+
+    public static void main(String[] args) {
+        ThreadLocal<String> threadLocal = new ThreadLocal<String>();
+
+        Thread worker1 = new Thread(new Worker(threadLocal, "worker1"));
+        Thread worker2 = new Thread(new Worker(threadLocal, "worker2"));
+        Thread worker3 = new Thread(new Worker(threadLocal, "worker3"));
+
+        worker1.start();
+        worker2.start();
+        worker3.start();
+    }
+
+}
---
+++ Threads/ThreadLocalVariable/src/edu/javacourse/threads/Worker.java
@@ -0,0 +1,30 @@
+package edu.javacourse.threads;
+
+import java.util.Random;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class Worker implements Runnable {
+
+    private ThreadLocal<String> threadLocal;
+    private String workerString;
+
+    public Worker(ThreadLocal<String> threadLocal, String workerString) {
+        this.threadLocal = threadLocal;
+        this.workerString = workerString;
+    }
+
+    @Override
+    public void run() {
+        threadLocal.set(workerString);
+
+        try {
+            Thread.sleep(new Random().nextInt(1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ThreadLocal value from " + workerString + " thread: " + threadLocal.get());
+    }
+}
