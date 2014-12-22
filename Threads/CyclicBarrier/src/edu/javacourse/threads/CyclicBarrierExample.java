6e4fa85d83acd4014645d417c4a3fb9acc0b95ff
Artem Pronchakov <artem.pronchakov@calisto.email>
2014-12-23 02:31:45 +0300

    Add Threads CyclicBarrier example

---
+++ Threads/CyclicBarrier/src/edu/javacourse/threads/CyclicBarrierExample.java
@@ -0,0 +1,24 @@
+package edu.javacourse.threads;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class CyclicBarrierExample {
+
+    public static void main(String[] args) {
+
+        CyclicBarrier barrier = new CyclicBarrier(3);
+
+        Thread worker1 = new Thread(new Worker(barrier), "worker1");
+        Thread worker2 = new Thread(new Worker(barrier), "worker2");
+        Thread worker3 = new Thread(new Worker(barrier), "worker3");
+
+        worker1.start();
+        worker2.start();
+        worker3.start();
+
+    }
+
+}
---
+++ Threads/CyclicBarrier/src/edu/javacourse/threads/Worker.java
@@ -0,0 +1,52 @@
+package edu.javacourse.threads;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class Worker implements Runnable {
+
+    private CyclicBarrier barrier;
+
+    public Worker(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        final String threadName = "Thread " + Thread.currentThread().getName();
+
+        System.out.println(threadName + " is starting work");
+
+        for (int i = 0; i < 5; i++) {
+            sleep(random.nextInt(5000));
+            System.out.println(threadName + " in on phase " + i + " and will be waiting for others");
+            awaitForAllOthers();
+            System.out.println(threadName + " is continuing it's work");
+        }
+
+        System.out.println(threadName + " has ended work");
+    }
+
+    private void awaitForAllOthers() {
+        try {
+            barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sleep(int random) {
+        try {
+            Thread.sleep(random);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
