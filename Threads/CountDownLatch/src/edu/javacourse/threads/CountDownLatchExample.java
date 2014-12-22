f874c4e9f4a73f500b6ebcf7477a0e8fa0bea35d
Artem Pronchakov <artem.pronchakov@calisto.email>
2014-12-22 21:57:07 +0300

    Add Threads CountDownLatch example

---
+++ Threads/CountDownLatch/src/edu/javacourse/threads/CountDownLatchExample.java
@@ -0,0 +1,41 @@
+package edu.javacourse.threads;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class CountDownLatchExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Random random = new Random();
+
+        CountDownLatch latch = new CountDownLatch(4);
+
+        Thread worker1 = new Thread(new Worker(latch), "worker1");
+        Thread worker2 = new Thread(new Worker(latch), "worker2");
+        Thread worker3 = new Thread(new Worker(latch), "worker3");
+
+        worker1.start();
+        worker2.start();
+        worker3.start();
+
+        Thread.sleep(2000);
+
+        System.out.println("Countdown begin:");
+
+        for (int i = 1; i < 5; i++) {
+
+            latch.countDown();
+            System.out.println("Countdown: " + i);
+
+            if (i < 4) Thread.sleep(random.nextInt(2000));
+        }
+
+        System.out.println("Countdown stopped");
+
+    }
+
+}
---
+++ Threads/CountDownLatch/src/edu/javacourse/threads/Worker.java
@@ -0,0 +1,29 @@
+package edu.javacourse.threads;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Artem Pronchakov | email/xmpp: artem.pronchakov@calisto.email
+ */
+public class Worker implements Runnable {
+
+    private CountDownLatch latch;
+
+    public Worker(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Worker " + Thread.currentThread().getName() + " is started");
+
+        try {
+            System.out.println("Worker " + Thread.currentThread().getName() + " is waiting for permission to start");
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Worker " + Thread.currentThread().getName() + " begin to work");
+    }
+}
